219f3ee6eddd3997556e4bae85cafd7ec4978aad
Andrew Haines <andrew.haines@red-badger.com>
2017-02-08 17:15:19 +0000

    Extract options into let

--- lein-test-report-junit-xml/src/lein_test_report_junit_xml/plugin.clj
+++ lein-test-report-junit-xml/src/lein_test_report_junit_xml/plugin.clj
@@ -2,12 +2,13 @@
   (:require [lein-test-report.utils :refer [add-profile]]))
 
 (defn middleware [project]
-  (let [output-path (or (System/getenv "TEST_REPORT_JUNIT_XML_OUTPUT_PATH")
-                        (-> project :test-report-junit-xml :output-path)
+  (let [options (:test-report-junit-xml project {})
+        output-path (or (System/getenv "TEST_REPORT_JUNIT_XML_OUTPUT_PATH")
+                        (:output-path options)
                         "target/TESTS-TestSuites.xml")]
     (add-profile project {:dependencies [['test-report-junit-xml "0.1.0-SNAPSHOT"]]
                           :plugins [['lein-test-report "0.1.0-SNAPSHOT"]]
                           :injections `[(require 'test-report-junit-xml.core)
                                         (require 'clojure.java.io)]
                           :test-report {:summarizers `[#(with-open [writer# (clojure.java.io/writer ~output-path)]
-                                                          (test-report-junit-xml.core/write writer# % ~(:test-report-junit-xml project {})))]}})))
+                                                          (test-report-junit-xml.core/write writer# % ~options))]}})))
--- lein-test-report/src/lein_test_report/plugin.clj
+++ lein-test-report/src/lein_test_report/plugin.clj
@@ -2,6 +2,7 @@
   (:require [lein-test-report.utils :refer [add-profile]]))
 
 (defn middleware [project]
-  (add-profile project {:dependencies [['test-report "0.1.0-SNAPSHOT"]]
-                        :injections `[(require 'test-report.core)
-                                      (test-report.core/activate ~(:test-report project {}))]}))
+  (let [options (:test-report project {})]
+    (add-profile project {:dependencies [['test-report "0.1.0-SNAPSHOT"]]
+                          :injections `[(require 'test-report.core)
+                                        (test-report.core/activate ~options)]})))
