d36370b2f1d7dd2fc66a881d096df5f4d77e4ac7
Andrew Haines <andrew.haines@red-badger.com>
2017-02-09 12:23:30 +0000

    Add description

--- lein-test-report/project.clj
+++ lein-test-report/project.clj
@@ -1,5 +1,6 @@
 (defproject lein-test-report "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Lein plugin providing hooks to customise clojure.test output"
   :url "https://github.com/redbadger/test-report"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
--- test-report/project.clj
+++ test-report/project.clj
@@ -1,6 +1,7 @@
 (defproject test-report "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Library providing hooks to customise clojure.test output"
   :url "https://github.com/redbadger/test-report"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [robert/hooke "1.3.0"]])
