38f53e1e6cb4b04d07013087dac60152ce434835
Andrew Haines <andrew.haines@red-badger.com>
2017-02-14 15:40:00 +0000

    Package test-report as a shaded uberjar

--- test-report/project.clj
+++ test-report/project.clj
@@ -1,7 +1,12 @@
-(defproject test-report "0.1.0"
+(defproject test-report "0.2.0-SNAPSHOT"
   :description "Library providing hooks to customise clojure.test output"
   :url "https://github.com/redbadger/test-report"
   :scm {:dir ".."}
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [robert/hooke "1.3.0"]])
+  :plugins [[lein-shade "0.1.0"]]
+  :profiles {:uberjar {:aot :all}
+             :provided {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :shaded {:dependencies [[robert/hooke "1.3.0"]]
+                      :shade {:namespaces [robert]}}
+             :default [:leiningen/default :shaded]}
+  :aliases {"deploy" ["deploy-shaded-jar" "clojars"]})
