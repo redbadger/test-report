34306104021251b6c7d379584e2d189379597430
Andrew Haines <andrew.haines@red-badger.com>
2017-02-15 11:41:26 +0000

    Update shade plugin

--- test-report/project.clj
+++ test-report/project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/redbadger/test-report"
   :scm {:dir ".."}
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-shade "0.2.0"]]
+  :plugins [[lein-shade "0.3.0"]]
   :profiles {:uberjar {:aot :all}
              :provided {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :shaded {:dependencies [[robert/hooke "1.3.0"]]
